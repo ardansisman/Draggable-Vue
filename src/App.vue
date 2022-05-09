3ba48f4f0a15d180d6e46415b44889ac169f4105
ardansisman <ardann.68@gmail.com>
2022-05-09 11:55:30 +0300

    --comm

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+  <draggable v-model="myArray">
+    <transition-group>
+      <div class="drop-zone" v-for="element in myArray" :key="element.id">
+        {{ element.name }}
+      </div>
+     
+    </transition-group>
+    
+  </draggable>
+  <h1>{{myArray}}</h1>
+</div>
+
+
+</template>
+
+<script>
+import draggable from "vuedraggable";
+
+export default {
+  data() {
+    return {
+      myArray: [
+        { id: 1, name: "soner" },
+        { id: 2, name: "ardan" },
+        { id: 3, name: "şişman" },
+      ],
+    };
+  },
+  components: {
+    draggable,
+  },
+};
+</script>
+
+<style>
+.drop-zone {
+  background-color: #eee;
+  margin-bottom: 10px;
+  padding: 10px;
+  cursor: pointer;
+}
+</style>
